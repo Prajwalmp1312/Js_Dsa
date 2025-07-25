50af06a5619340586d879cf7f1366b10d94098f3
prajwalprabhakar <prajwalprabhakar1312@gmail.com>
2025-07-25 20:01:25 +0530

    added bubblesort

---
+++ BubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr){
+    let swapped
+    do{
+        swapped=false
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i]>arr[i+1]){
+                let temp=arr[i]
+                arr[i]=arr[i+1]
+                arr[i+1]=temp
+                swapped=true
+            }
+            
+        }
+    }while(swapped)
+}
+
+const arr=[12,78,-8,13,-6,25]
+bubbleSort(arr)
+console.log(arr);
